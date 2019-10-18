11d50ced1521a34ef1c4853d638fda43f35377af
Aaron-Doran <wesley.gossett@gmail.com>
2019-10-18 13:48:29 -0400

    Did stretch goal in objects-arrays.js converting my array methods into ES6 syntax

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -268,7 +268,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = zooAnimals.reduce(function(total, curPop) {
+const populationTotal = zooAnimals.reduce((total, curPop) => {
   return total + curPop.population;
 }, 0);
 
